4b7c954f7140aeaf79ff864eb436dddca05c7ad8
Iris Sá <irisamsa@gmail.com>
2024-07-07 19:39:25 -0300

    Criando custon commands para testes e2e

---
+++ cypress/support/commands.js
@@ -0,0 +1,45 @@
+Cypress.Commands.add('visitWikipedia', () => {
+    cy.visit('')
+})
+
+Cypress.Commands.add('elementExistsByWiki', (id) => {
+    cy.get(`${id}`).should('exist')
+})
+
+Cypress.Commands.add('search', (term) => {
+    cy.get('input[name="search"]').type(`${term}{enter}`)
+})
+
+Cypress.Commands.add('getUser', (userId) => {
+    const query = `
+        query GetUser($id: ID!) {
+            user(id: $id) {
+                id
+                username
+                email
+                address {
+                    geo {
+                        lat
+                        lng
+                    }
+                }
+            }
+        }
+    `;
+
+    cy.request({
+        method: 'POST',
+        url: 'https://graphqlzero.almansi.me/api',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: {
+            query,
+            variables: { id: userId },
+        },
+    }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body.data).to.not.be.null;
+        return response.body.data;
+    });
+});
